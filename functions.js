097f314c913d30823b12f1890449203302ca11ec
Tamara <tklong04.27@gmail.com>
2019-01-24 17:09:02 -0500

    Adding .removeClass functionality to hamburger menu

--- functions.js
+++ functions.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
 
-$('.hamburger-icon').click(function(x){
+$('.hamburger-icon').click(function(){
   $('.mobile-menu').addClass('open');
+  $('.bar-one, .bar-two, .bar-three').toggleClass('change');
+
+});
+
+// function myFunction() {
+//  var element = document.getElementById(".mobile-menu");
+//  element.classList.remove(".open");
+// };
+
+$('.hamburger-icon').click(function(){
+  $('.mobile-menu').removeClass('open');
   $('.bar-one, .bar-two, .bar-three').toggleClass('change');
 
 });
