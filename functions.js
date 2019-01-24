c4091ee15c256900b586efdc2f8065152f3adbf2
Tamara <tklong04.27@gmail.com>
2019-01-24 15:08:42 -0500

    Mobile menu shows up when hamburger menu is clicked

--- functions.js
+++ functions.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
 $('.hamburger-icon').click(function(){
-  $('.mobile-menu').addClass('.open');
+  // $('.mobile-menu').addClass('open');
+  $('.bar-one, .bar-two, bar-three').toggle('change');
+
 });
 
 });
