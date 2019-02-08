f83c4e79c7b13ff5a3e2eb4bbe155e17be31a32b
Tamara <tklong04.27@gmail.com>
2019-02-07 20:50:28 -0500

    Added favicon, reduced image sizes, updated title tags, fixed contact page

--- js/functions.js
+++ js/functions.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
     $('.bar-one, .bar-two, .bar-three, .hamburger-icon').toggleClass('change');
     $('body').toggleClass("fixedPosition");
   });
+  $(window).resize(function() {
+    if ($(window).width() > 800 && $('.mobile-menu').hasClass('open')) {
+      $('.mobile-menu').toggleClass('open');
+      $('.bar-one, .bar-two, .bar-three, .hamburger-icon').toggleClass('change');
+      $('body').toggleClass("fixedPosition");
+    }
+  });
 });
